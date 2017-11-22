2cef170bc60032175af209e3709dac9d603e87c9
Ula2017 <urszulasobon1996@gmail.com>
2017-11-22 21:38:58 +0100

    test file

---
+++ test/test.ino
@@ -0,0 +1,53 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+
+
+
+#include<SPI.h>
+
+#define ONE_WIRE_BUS 2 
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+
+// ce, csn pins
+RF24 radio(9, 10);
+ 
+void setup(void){
+
+  // radio initialization
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x76);
+  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
+
+  // temperature sensor initialization
+  sensors.begin(); 
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+ 
+}
+ 
+void loop(void){
+
+  sensors.requestTemperatures();
+    char *text;
+    text = (char *)malloc(32);
+    
+    float zmienna = sensors.getTempCByIndex(0);
+    Serial.write("tekst %lf",zmienna);
+  radio.write(&zmienna, sizeof(text));
+  delay(1000);
+ 
+}
+
